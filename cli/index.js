36d8130bafcf548eefb33d847b89169d5db77a0f
Alec <alecdux@gmail.com>
2025-04-10 11:57:14 +0800

    chore: cahnge project name

--- cli/index.js
+++ cli/index.js
@@ -1,34 +1,27 @@
 #!/usr/bin/env node
-
 import { Command } from "commander";
 import { createRequire } from "node:module";
-
 const require = createRequire(import.meta.url);
 const version = require("../package.json").version;
-
 const program = new Command();
-
 program
-  .name("sblog")
-  .description("A simple blog application")
-  .version(version);
-
+    .name("sblog")
+    .description("A simple blog application")
+    .version(version);
 // build 子命令
 program
-  .command('build')
-  .description('Build the project')
-  .action(() => {
+    .command('build')
+    .description('Build the project')
+    .action(() => {
     console.log('Building...');
     // 你的构建逻辑
-  });
-
+});
 // publish 子命令
 program
-  .command('publish')
-  .description('Publish the project')
-  .action(() => {
+    .command('publish')
+    .description('Publish the project')
+    .action(() => {
     console.log('Publishing...');
     // 你的发布逻辑
-  });
-
+});
 program.parse(process.argv);
